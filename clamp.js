8263c1a86fd307922dcb2c16e8b0be80a2fb4e32
Joseph J. Schmitt <joe@vimeo.com>
2011-01-07 12:25:08 -0500

    v0.1. Works with native webkit css clamping. Recognizes an integer to use as a clamp or a value of 'auto' where it will figure out how much to clamp based on a static height

---
+++ clamp.js
@@ -0,0 +1,135 @@
+/*!
+* Clamp.JS 0.1
+*
+* Copyright 2010, Joseph Schmitt http://reusablebits.com, http://josephschmitt.me
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*/
+
+(function(){
+    /**
+     * Return the current style for an element.
+     * @param {HTMLElement} elem The element to compute.
+     * @param {string} prop The style property.
+     * @returns {number}
+     */
+    function computeStyle(elem, prop) {
+        var win = window;
+        if (!win.getComputedStyle) {
+            win.getComputedStyle = function(el, pseudo) {
+                this.el = el;
+                this.getPropertyValue = function(prop) {
+                    var re = /(\-([a-z]){1})/g;
+                    if (prop == 'float') prop = 'styleFloat';
+                    if (re.test(prop)) {
+                        prop = prop.replace(re, function () {
+                            return arguments[2].toUpperCase();
+                        });
+                    }
+                    return el.currentStyle && el.currentStyle[prop] ? el.currentStyle[prop] : null;
+                }
+                return this;
+            }
+        }
+
+        return win.getComputedStyle(elem, null).getPropertyValue(prop);
+    }
+
+    /**
+     * Clamps a text node.
+     * @param {HTMLElement} element. Element containing the text node to clamp.
+     * @param {Object} options. Options to pass to the clamper.
+     */
+    function clamp(element, options) {
+        options = options || {};
+
+        var self = this,
+            opt = {
+                clamp: options.clamp        || 2
+            },
+            original = element.innerHTML,
+
+            hasNativeClamp = typeof(element.style.webkitLineClamp) != 'undefined',
+            clampValue = opt.clamp === 'auto' ? getMaxLines() : opt.clamp;
+
+
+    // UTILITY FUNCTIONS __________________________________________________________
+
+        /**
+         * Returns the maximum number of lines of text that should be rendered based
+         * on the current height of the element and the line-height of the text.
+         */
+        function getMaxLines() {
+            var availHeight = element.clientHeight,
+                lineHeight = getLineHeight(element);
+
+            return Math.max(Math.floor(availHeight/lineHeight), 0);
+        }
+
+        /**
+         * Returns the maximum height a given element should have based on the line-
+         * height of the text and the given clamp value.
+         */
+        function getMaxHeight(clmp) {
+            var lineHeight = getLineHeight(element);
+            return lineHeight * clmp;
+        }
+
+        /**
+         * Returns the line-height of an element as an integer.
+         */
+        function getLineHeight(elem) {
+            var lh = computeStyle(elem, 'line-height');
+            if (lh == 'normal') {
+                // Normal line heights vary from browser to browser. The spec recommends
+                // a value between 1.0 and 1.2 of the font size. Using 1.1 to split the diff.
+                lh = parseInt(computeStyle(elem, 'font-size')) * 1.1;
+            }
+            return parseInt(lh);
+        }
+
+
+    // MEAT AND POTATOES (MMMM, POTATOES...) ______________________________________
+
+        /**
+         * Removes one character at a time from the text until its width or
+         * height is beneath the passed-in max param.
+         */
+        function truncate() {
+            if (element[property] <= max ) {
+                return false;
+            }
+
+            while (self[property] > max) {
+                self.innerText = self.innerText.substr(0, self.innerText.length-1);
+                if (self.innerText == '' || self.innerText.length == 0) {
+                    return false;
+                }
+            }
+
+            applyEllipsis();
+        }
+
+        function applyEllipsis() {
+            self.innerText = self.innerText.substr(0, self.innerText.length-3) + '...';
+        }
+
+
+    // CONSTRUCTOR ________________________________________________________________
+
+        if (hasNativeClamp) {
+            var sty = element.style;
+
+            sty.overflow = 'hidden';
+            sty.textOverflow = 'ellipsis';
+            sty.webkitBoxOrient = 'vertical';
+            sty.display = '-webkit-box';
+            sty.webkitLineClamp = clampValue;
+        }
+        else {
+
+        }
+    }
+    
+    window.$clamp = clamp;
+})();
