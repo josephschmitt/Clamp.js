85e3c97d2a7eaafe47d9d41049abbf2981a96a0a
adr-enal-in <dakotah@gophilosophie.com>
2013-03-20 15:35:44 -0700

    Moved internal utilities into publicly available object.
    Updated README with code sample.

--- README.markdown
+++ README.markdown
@@ -1,4 +1,4 @@
-Clamps (ie. cuts off) an HTML element's content by adding ellipsis to it if the 
+Clamps (ie. cuts off) an HTML element's content by adding ellipsis to it if the
 content inside is too long.
 
 
@@ -19,19 +19,19 @@ $clamp(myParagraph, {clamp: '35px'});
 </pre>
 
 The $clamp method is the primary way of interacting with Clamp.js, and it takes two
-arguments. The first is the element which should be clamped, and the second is an
+arguments. The first is the DOM object (`$("p")[0]`) which should be clamped, and the second is an
 Object with options in JSON notation.
 
 
 # Options
 
-**clamp** _(Number | String | 'auto')_. This controls where and when to clamp the 
+**clamp** _(Number | String | 'auto')_. This controls where and when to clamp the
 text of an element. Submitting a number controls the number of lines that should
 be displayed. Second, you can submit a CSS value (in px or em) that controls the
 height of the element as a String. Finally, you can submit the word 'auto' as a string.
 Auto will try to fill up the available space with the content and then automatically
-clamp once content no longer fits. This last option should only be set if a static 
-height is being set on the element elsewhere (such as through CSS) otherwise no 
+clamp once content no longer fits. This last option should only be set if a static
+height is being set on the element elsewhere (such as through CSS) otherwise no
 clamping will be done.
 
 **useNativeClamp** _(Boolean)_. Enables or disables using the native -webkit-line-clamp
@@ -46,10 +46,30 @@ can use. For example, it you pass an array of ['.', ',', ' '] then it will first
 sentences, then remove comma-phrases, and remove words, and finally remove individual
 characters to try and find the correct height. This will lead to increased performance
 and less looping when removing larger pieces of text (such as in paragraphs). The default
-is set to remove sentences (periods), hypens, en-dashes, em-dashes, and finally words 
+is set to remove sentences (periods), hypens, en-dashes, em-dashes, and finally words
 (spaces). Removing by character is always enabled as the last attempt no matter what
 is submitted in the array.
 
 **animate** _(Boolean)_. Silly little easter-egg that, when set to true, will animate
 removing individual characters from the end of the element until the content fits.
 Defaults to false.
+
+
+# Utilities
+
+Internal clamp utilities are publicly exposed through the global `clampUtils`.
+
+For instance, you can use `clampUtils.getLineHeight()` to get the line height for the DOM object and use that to determine what to do another element.
+
+<pre>
+var headline = $("h1")[0];
+$clamp(headline, { lines: 2 });
+
+var subheadline = $("h3")[0];
+if (clampUtils.getLineHeight(headline) * 2 === headline.clientHeight) {
+	subheadNumLines = 1;
+} else {
+	subheadNumLines = 2;
+}
+$clamp(subheadline, { lines: dekLines});
+</pre>
--- clamp.js
+++ clamp.js
@@ -1,5 +1,5 @@
 /*!
-* Clamp.js 0.4
+* Clamp.js 0.5
 *
 * Copyright 2011, Joseph Schmitt http://reusablebits.com, http://josephschmitt.me
 * Released under the WTFPL license
@@ -12,13 +12,75 @@
      * @param {HTMLElement} element. Element containing the text node to clamp.
      * @param {Object} options. Options to pass to the clamper.
      */
+
+    var clampUtils = {
+      /**
+      * Returns the line-height of an element as an integer.
+      */
+      getLineHeight: function (elem) {
+        var lh = this.computeStyle(elem, 'line-height');
+        if (lh == 'normal') {
+            // Normal line heights vary from browser to browser. The spec recommends
+            // a value between 1.0 and 1.2 of the font size. Using 1.1 to split the diff.
+            lh = parseInt(this.computeStyle(elem, 'font-size')) * 1.2;
+        }
+        return parseInt(lh);
+      },
+
+      /**
+       * Return the current style for an element.
+       * @param {HTMLElement} elem The element to compute.
+       * @param {string} prop The style property.
+       * @returns {number}
+       */
+      computeStyle: function (elem, prop) {
+          if (!window.getComputedStyle) {
+              window.getComputedStyle = function(el, pseudo) {
+                  this.el = el;
+                  this.getPropertyValue = function(prop) {
+                      var re = /(\-([a-z]){1})/g;
+                      if (prop == 'float') prop = 'styleFloat';
+                      if (re.test(prop)) {
+                          prop = prop.replace(re, function () {
+                              return arguments[2].toUpperCase();
+                          });
+                      }
+                      return el.currentStyle && el.currentStyle[prop] ? el.currentStyle[prop] : null;
+                  }
+                  return this;
+              }
+          }
+
+          return window.getComputedStyle(elem, null).getPropertyValue(prop);
+      },
+
+      /**
+       * Returns the maximum number of lines of text that should be rendered based
+       * on the current height of the element and the line-height of the text.
+       */
+      getMaxLines: function (element, height) {
+          var availHeight = height || element.clientHeight,
+              lineHeight = clampUtils.getLineHeight(element);
+          return Math.max(Math.floor(availHeight/lineHeight), 0);
+      },
+
+      /**
+       * Returns the maximum height a given element should have based on the line-
+       * height of the text and the given clamp value.
+       */
+      getMaxHeight: function (element, clmp) {
+          var lineHeight = clampUtils.getLineHeight(element);
+          return lineHeight * clmp;
+      }
+    }
+
     function clamp(element, options) {
         options = options || {};
 
         var self = this,
             win = window,
             opt = {
-                clamp:              options.clamp || 2,
+                lines:              options.lines || 2,
                 useNativeClamp:     typeof(options.useNativeClamp) != 'undefined' ? options.useNativeClamp : true,
                 splitOnChars:       options.splitOnChars || ['.', '-', '–', '—', ' '], //Split on sentences (periods), hypens, en-dashes, em-dashes, and words (spaces).
                 animate:            options.animate || false
@@ -28,71 +90,11 @@
             original = element.innerHTML,
 
             supportsNativeClamp = typeof(element.style.webkitLineClamp) != 'undefined',
-            clampValue = opt.clamp,
+            clampValue = opt.lines,
             isCSSValue = clampValue.indexOf && (clampValue.indexOf('px') > -1 || clampValue.indexOf('em') > -1);
 
 
 // UTILITY FUNCTIONS __________________________________________________________
-
-        /**
-         * Return the current style for an element.
-         * @param {HTMLElement} elem The element to compute.
-         * @param {string} prop The style property.
-         * @returns {number}
-         */
-        function computeStyle(elem, prop) {
-            if (!win.getComputedStyle) {
-                win.getComputedStyle = function(el, pseudo) {
-                    this.el = el;
-                    this.getPropertyValue = function(prop) {
-                        var re = /(\-([a-z]){1})/g;
-                        if (prop == 'float') prop = 'styleFloat';
-                        if (re.test(prop)) {
-                            prop = prop.replace(re, function () {
-                                return arguments[2].toUpperCase();
-                            });
-                        }
-                        return el.currentStyle && el.currentStyle[prop] ? el.currentStyle[prop] : null;
-                    }
-                    return this;
-                }
-            }
-
-            return win.getComputedStyle(elem, null).getPropertyValue(prop);
-        }
-
-        /**
-         * Returns the maximum number of lines of text that should be rendered based
-         * on the current height of the element and the line-height of the text.
-         */
-        function getMaxLines(height) {
-            var availHeight = height || element.clientHeight,
-                lineHeight = getLineHeight(element);
-
-            return Math.max(Math.floor(availHeight/lineHeight), 0);
-        }
-
-        /**
-         * Returns the maximum height a given element should have based on the line-
-         * height of the text and the given clamp value.
-         */
-        function getMaxHeight(clmp) {
-            var lineHeight = getLineHeight(element);
-            return lineHeight * clmp;
-        }
-
-        /**
-         * Returns the line-height of an element as an integer.
-         */
-        function getLineHeight(elem) {
-            var lh = computeStyle(elem, 'line-height');
-            if (lh == 'normal') {
-                // Normal line heights vary from browser to browser. The spec recommends
-                // a value between 1.0 and 1.2 of the font size. Using 1.1 to split the diff.
-                lh = parseInt(computeStyle(elem, 'font-size')) * 1.2;
-            }
-            return parseInt(lh);
-        }
 
 
 // MEAT AND POTATOES (MMMM, POTATOES...) ______________________________________
@@ -100,7 +102,7 @@
             splitChar = splitOnChars[0],
             chunks,
             lastChunk;
-        
+
         /**
          * Gets an element's last child. That may be another node or a node's contents.
          */
@@ -119,14 +121,14 @@
                 return elem.lastChild;
             }
         }
-        
+
         /**
          * Removes one character at a time from the text until its width or
          * height is beneath the passed-in max param.
          */
         function truncate(target, maxHeight) {
             if (!maxHeight) {return;}
-            
+
             /**
              * Resets global variables.
              */
@@ -136,9 +138,9 @@
                 chunks = null;
                 lastChunk = null;
             }
-            
+
             var nodeValue = target.nodeValue.replace(/…/, '');
-            
+
             //Grab the next chunks
             if (!chunks) {
                 //If there are more characters to try, grab the next one
@@ -149,16 +151,14 @@
                 else {
                     splitChar = '';
                 }
-                
+
                 chunks = nodeValue.split(splitChar);
             }
-            
+
             //If there are chunks left to remove, remove the last one and see if
             // the nodeValue fits.
             if (chunks.length > 1) {
-                // console.log('chunks', chunks);
                 lastChunk = chunks.pop();
-                // console.log('lastChunk', lastChunk);
                 applyEllipsis(target, chunks.join(splitChar));
             }
             //No more chunks can be removed using this character
@@ -188,11 +188,11 @@
                 if (splitChar == '') {
                     applyEllipsis(target, '');
                     target = getLastChild(element);
-                    
+
                     reset();
                 }
             }
-            
+
             //If you get here it means still too big, let's keep truncating
             if (opt.animate) {
                 setTimeout(function() {
@@ -203,7 +203,7 @@
                 truncate(target, maxHeight);
             }
         }
-        
+
         function applyEllipsis(elem, str) {
             elem.nodeValue = str + '…';
         }
@@ -212,10 +212,10 @@
 // CONSTRUCTOR ________________________________________________________________
 
         if (clampValue == 'auto') {
-            clampValue = getMaxLines();
+            clampValue = clampUtils.getMaxLines(element);
         }
         else if (isCSSValue) {
-            clampValue = getMaxLines(parseInt(clampValue));
+            clampValue = clampUtils.getMaxLines(element, parseInt(clampValue));
         }
 
         if (supportsNativeClamp && opt.useNativeClamp) {
@@ -226,11 +226,11 @@
             sty.webkitLineClamp = clampValue;
 
             if (isCSSValue) {
-                sty.height = opt.clamp + 'px';
+                sty.height = opt.lines + 'px';
             }
         }
         else {
-            var height = getMaxHeight(clampValue);
+            var height = clampUtils.getMaxHeight(element, clampValue);
             if (height <= element.clientHeight) {
                 truncate(getLastChild(element), height);
             }
@@ -238,4 +238,6 @@
     }
 
     window.$clamp = clamp;
+    window.clampUtils = clampUtils;
 })();
+
